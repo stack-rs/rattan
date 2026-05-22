547654d83348c37635623d71ff855ec34fff148f
un-lock-able <gaogao174561@outlook.com>
2026-05-22 14:43:58 +0800

    refactor: split queue module into multiple files

---
+++ rattan-core/src/cells/bandwidth/queue/droptail.rs
@@ -0,0 +1,147 @@
+use std::collections::VecDeque;
+
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+use tracing::debug;
+
+#[cfg(feature = "serde")]
+use super::serde_default;
+use super::{BwType, PacketQueue};
+use crate::cells::Packet;
+
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[derive(Debug, Default, Clone)]
+pub struct DropTailQueueConfig {
+    pub packet_limit: Option<usize>, // None means unlimited
+    pub byte_limit: Option<usize>,   // None means unlimited
+    #[cfg_attr(
+        feature = "serde",
+        serde(default, skip_serializing_if = "serde_default")
+    )]
+    pub bw_type: BwType,
+}
+
+impl DropTailQueueConfig {
+    pub fn new<A: Into<Option<usize>>, B: Into<Option<usize>>>(
+        packet_limit: A,
+        byte_limit: B,
+        bw_type: BwType,
+    ) -> Self {
+        Self {
+            packet_limit: packet_limit.into(),
+            byte_limit: byte_limit.into(),
+            bw_type,
+        }
+    }
+}
+
+impl<P> From<DropTailQueueConfig> for DropTailQueue<P> {
+    fn from(config: DropTailQueueConfig) -> Self {
+        DropTailQueue::new(config)
+    }
+}
+
+#[derive(Debug)]
+pub struct DropTailQueue<P> {
+    queue: VecDeque<P>,
+    bw_type: BwType,
+    packet_limit: Option<usize>,
+    byte_limit: Option<usize>,
+    now_bytes: usize,
+}
+
+impl<P> DropTailQueue<P> {
+    pub fn new(config: DropTailQueueConfig) -> Self {
+        let packet_limit = config.packet_limit;
+        let byte_limit = config.byte_limit;
+        debug!(?config, "New DropTailQueue");
+        Self {
+            queue: VecDeque::new(),
+            bw_type: config.bw_type,
+            packet_limit,
+            byte_limit,
+            now_bytes: 0,
+        }
+    }
+}
+
+impl<P> Default for DropTailQueue<P> {
+    fn default() -> Self {
+        Self::new(DropTailQueueConfig::default())
+    }
+}
+
+impl<P> PacketQueue<P> for DropTailQueue<P>
+where
+    P: Packet,
+{
+    type Config = DropTailQueueConfig;
+
+    fn configure(&mut self, config: Self::Config) {
+        self.packet_limit = config.packet_limit;
+        self.byte_limit = config.byte_limit;
+        self.bw_type = config.bw_type;
+    }
+
+    fn is_zero_buffer(&self) -> bool {
+        self.packet_limit.is_some_and(|limit| limit == 0)
+            || self.byte_limit.is_some_and(|limit| limit == 0)
+    }
+
+    fn enqueue(&mut self, packet: P) {
+        if self
+            .packet_limit
+            .is_none_or(|limit| self.queue.len() < limit)
+            && self.byte_limit.is_none_or(|limit| {
+                self.now_bytes + packet.l3_length() + self.bw_type.extra_length() <= limit
+            })
+        {
+            self.now_bytes += packet.l3_length() + self.bw_type.extra_length();
+            self.queue.push_back(packet);
+        } else {
+            #[cfg(test)]
+            tracing::trace!(
+                queue_len = self.queue.len(),
+                now_bytes = self.now_bytes,
+                header = ?format!("{:X?}", &packet.as_slice()[0..std::cmp::min(56, packet.length())]),
+                "Drop packet(l3_len: {}, extra_len: {}) when enqueuing", packet.l3_length(), self.bw_type.extra_length()
+            );
+        }
+    }
+
+    fn dequeue(&mut self) -> Option<P> {
+        match self.queue.pop_front() {
+            Some(packet) => {
+                self.now_bytes -= packet.l3_length() + self.bw_type.extra_length();
+                Some(packet)
+            }
+            None => None,
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.queue.is_empty()
+    }
+
+    #[inline(always)]
+    fn get_extra_length(&self) -> usize {
+        self.bw_type.extra_length()
+    }
+
+    fn get_front_size(&self) -> Option<usize> {
+        self.queue
+            .front()
+            .map(|packet| self.get_packet_size(packet))
+    }
+
+    fn length(&self) -> usize {
+        self.queue.len()
+    }
+
+    fn retain<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&P) -> bool,
+    {
+        self.queue.retain(|packet| f(packet));
+    }
+}
